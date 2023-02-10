d1c2ebc338dcf963835872b31ab48d49b85d17f1
elvis <cenelvis@gmail.com>
2023-02-10 16:50:54 -0500

    61. Constructor Func & this Keyword

--- #5 Classes & Interfaces/src/app.ts
+++ #5 Classes & Interfaces/src/app.ts
@@ -1,4 +1,5 @@
 // 59. Creating a First Class
+// 61. Constructor Functions & The "this" Keyword
 
 class Department {
   name: string;
@@ -6,4 +7,18 @@ class Department {
   constructor(n: string) {
     this.name = n;
   }
+
+  // this: Department => when "describe()" is called it will always refer to the original ".this"
+  // Whenever "describe()" is called, it expects a "name:"
+  describe(this: Department) {
+    console.log("Department: " + this.name);
+  }
 }
+
+const accounting = new Department("Accounting");
+
+accounting.describe();
+
+const accountingCopy = { name: "Yo", describe: accounting.describe };
+
+accountingCopy.describe();
